a2cb303bb3af9879110878d34a04aca0f2976c6d
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-09-17 15:02:20 -0600

    fetch all traveler data

--- src/fetch-requests.js
+++ src/fetch-requests.js
@@ -1,3 +1,16 @@
-const handleData =  {
+let fetchRequests = {
+  getAllTravelerData() {
+         fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers')
+          .then(response => response.json())
+          .then(data => {
+            console.log('data',data)
+          })
+          .then(data => {
+            id = data.id,
+            name = data.name,
+            travelerType = data.travelerType
+          })
+  }
 
+ getTripData(){
 }
