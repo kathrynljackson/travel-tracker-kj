6cfa9f58e1fb0f3344187df91fc40c16cf0b9f81
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-09-18 09:49:33 -0600

    Update Traveler class

--- src/traveler.js
+++ src/traveler.js
@@ -1,7 +1,12 @@
 class Traveler {
-  constructor(id, name, travelerType) {
-    this.id = id;
-    this.name = name;
-    this.travelerType = travelerType;
+  constructor(travelerData) {
+    this.travelerData = travelerData
+    this.id = travelerData.id;
+    this.name = travelerData.name;
+    this.travelerType = travelerData.travelerType;
+    this.trips = [];
+    this.amountSpent = 0;
   }
 }
+
+export default Traveler;
