d67512651b1957bd15921135108a9a5757a1ed06
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-09-18 11:03:52 -0600

    Add/pass testing for Traveler class construction

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -2,9 +2,11 @@ import { expect } from 'chai';
 import Traveler from '../src/traveler.js'
 
 describe('Traveler', function() {
-  let traveler;
+ let traveler;
+ let travelerData;
 
-  beforeEach(function() {
+
+  beforeEach(() => {
     let sampleTravelers = [
       {
         "id": 1,
@@ -31,10 +33,27 @@ describe('Traveler', function() {
         "name": "Tiffy Grout",
         "travelerType": "thrill-seeker"
         }
-    ]
+    ];
+
+
+    travelerData = sampleTravelers;
+    traveler = new Traveler(travelerData);
   })
+
   it('should be a function', () => {
-    expect('Traveler').to.be.a('function');
+    expect(Traveler).to.be.a('function');
+  })
+
+  it('should be an instance of Traveler', () => {
+    expect(traveler).to.be.an.instanceOf(Traveler);
+  })
+
+  it('should take in the traveler\'s data as an argument', () => {
+    traveler = new Traveler(travelerData[0]);
+    expect(traveler.travelerData).to.equal(travelerData[0]);
+    expect(traveler.id).to.equal(1);
+    expect(traveler.name).to.equal("Ham Leadbeater");
+    expect(traveler.travelerType).to.equal("relaxer");
   })
 
 })
