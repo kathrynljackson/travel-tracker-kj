eee8dd4b58bd8d4271c1e72eb3a69ea12366e072
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-09-19 14:07:53 -0600

    Initial commit, create Trips class

---
+++ src/trips.js
@@ -0,0 +1,11 @@
+class Trips {
+ constructor(destinationData) {
+  this.destinationData = destinationData,
+  this.id = destinationData.id,
+  this.destinationID = destinationData.destinationID,
+  this.travlers = destinationID.travelers,
+  this.date = destinationData.date,
+  this.status = destinationData.status,
+  this.suggestedActivities = []
+ }
+}
