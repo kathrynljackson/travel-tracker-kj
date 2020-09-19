ffde50daa1bc73b7d2389eb9e4c1ee66a83a5c37
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-09-18 19:41:50 -0600

    All data imported, can't access

--- src/fetch-requests.js
+++ src/fetch-requests.js
@@ -1,14 +1,67 @@
+import createData from './index.js'
+
 let fetchRequests = {
-  getData() {
-   return Promise.all)[
-     fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers'),
-     fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips'),
-     fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
-   ])
- }
+  getTravelerData() {
+    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers')
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .then(travelers => createData.createTravelerData(travelers))
+      .catch(err => console.log('ERROR IS OCCURING IN getTravelerData'))
+ },
 
+ getSingleTravelerData(id) {
+   fetch(`https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/${id}`)
+     .then(response = response.json())
+     .then(data => console.log(data))
+     .then(data => createData.createSingleTravelerData(data))
+     .catch(err => console.log('ERROR IS OCCURING IN getSingleTravelerData'))
+ },
 
- //getTripData(){
+  getTripData() {
+    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips')
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .then(data => createData.createTripData(data))
+      .catch(err => console.log('ERROR IS OCCURING IN getTripData'))
+  },
+
+  getDestinationData() {
+    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .then(data => createData.createDestinationData(data))
+    .catch(err => console.log('ERROR IS OCCURING IN getDestinationData'))
+  }
 }
+
+// import userStuff from './index.js';
+//
+// let fetcher = {
+//  fetchUser(id) {
+//     fetch(`https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/${id}`)
+//         .then(response => response.json())
+//         .then(data => userStuff.setUserData(data))
+//         .then(this.fetchTripsForAUser())
+//         .catch(err => console.log('err', err))
+// },
+//
+//  fetchTripsForAUser() {
+//     fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips')
+//         .then(response => response.json())
+//         .then(data => userStuff.setUserTrips(data.trips))
+//         // .then(data => console.log(data.trips.filter(trips => trips.userID === userData.id)))
+//         .catch(err => console.log('err', err))
+//     }
+// }
+
+// let userStuff = {
+//       setUserData(fetchedUserData) {
+//         userData = fetchedUserData
+//     },
+//     setUserTrips(fetchedTrips) {
+//         //  console.log(fetchedTrips.trips)
+//         userTrips = fetchedTrips.filter(fetchedTrip => fetchedTrip.userID === userData.id)
+//      }
+// }
 
 export default fetchRequests;
--- src/index.js
+++ src/index.js
@@ -7,4 +7,55 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+import Traveler from './traveler.js'
+import fetchRequests from './fetch-requests.js'
+
+let allTravelers;
+let allTrips;
+let allDestinations;
+let traveler;
+
+window.addEventListener('load', fetchRequests.getData);
+window.addEventListener('load', retrieveData);
+//window.addEventListener('load', consoleLog);
+//window.addEventListener('load', generateTraveler);
+
+function retrieveData(){
+  fetchRequests.getTravelerData();
+  fetchRequests.getTripData();
+  fetchRequests.getDestinationData();
+}
+
+let createData = {
+  createSingleTravelerData(travelerData) {
+    traveler = new Traveler(travelerData)
+  },
+
+  createTravelerData(travelerData) {
+    allTravelers = travelerData;
+  },
+
+  createTripData(tripData) {
+    allTrips = tripData;
+  },
+
+  createDestinationData(destinationData) {
+    allDestinations = destinationData;
+  }
+}
+
+function consoleLog(){
+  console.log('allTravelers', allTravelers);
+  console.log('allTrips', allTrips)
+  console.log('allDestinations', allDestinations)
+  console.log('fetch function', fetchRequests)
+}
+
+consoleLog();
+
+
+// function generateTraveler() {
+//   traveler = new Traveler(allTravelers[Math.floor(Math.random() * allTravelers.length)]);
+// }
+
+export default createData;
