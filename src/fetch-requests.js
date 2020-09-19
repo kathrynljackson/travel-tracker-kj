8a2d904c36466720cc58b3e8c2086049277501e4
Kathryn Jackson <65988644+kathrynljackson@users.noreply.github.com>
2020-09-18 19:51:37 -0600

    Update fetch-requests.js

--- src/fetch-requests.js
+++ src/fetch-requests.js
@@ -34,34 +34,4 @@ let fetchRequests = {
   }
 }
 
-// import userStuff from './index.js';
-//
-// let fetcher = {
-//  fetchUser(id) {
-//     fetch(`https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/${id}`)
-//         .then(response => response.json())
-//         .then(data => userStuff.setUserData(data))
-//         .then(this.fetchTripsForAUser())
-//         .catch(err => console.log('err', err))
-// },
-//
-//  fetchTripsForAUser() {
-//     fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips')
-//         .then(response => response.json())
-//         .then(data => userStuff.setUserTrips(data.trips))
-//         // .then(data => console.log(data.trips.filter(trips => trips.userID === userData.id)))
-//         .catch(err => console.log('err', err))
-//     }
-// }
-
-// let userStuff = {
-//       setUserData(fetchedUserData) {
-//         userData = fetchedUserData
-//     },
-//     setUserTrips(fetchedTrips) {
-//         //  console.log(fetchedTrips.trips)
-//         userTrips = fetchedTrips.filter(fetchedTrip => fetchedTrip.userID === userData.id)
-//      }
-// }
-
 export default fetchRequests;
