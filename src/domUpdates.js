37caedaf10a34ba1593c7281a5b6acb791374eb9
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-09-21 07:54:54 -0600

    Add displayPendingTrips(), displayApprovedTrips(), and displayCostSpent()

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,7 +1,9 @@
 import Traveler from './traveler.js';
 import traveler from './index.js';
 import travelerGreeting from './index.js';
-
+import Destination from './destination.js';
+import destination from './index.js';
+import createData from './index.js';
 
 
 let domUpdates = {
@@ -9,12 +11,60 @@ let domUpdates = {
   displayTravelerGreeting(travelerName) {
     document.querySelector('.traveler-dashboard-greeting').innerText = 'Welcome, '+travelerName+'!';
     console.log('displayTravelerGreeting is running');
+    //it needs to be travelerName, not traveler.name
  },
 
-  displayAllTrips(travelerTrips) {
-    document.querySelector('.upcoming-trips-info').innerHTML = travelerTrips+'\n';
+  displayAllTrips(traveler, travelerDestinations) {
+    let destination = new Destination(travelerDestinations);
+
+    let separateTrips = traveler.trips.forEach(trip => {
+      let destinationInfo = destination.getDestinationDetails(trip.destinationID);
+      document.querySelector('.upcoming-trips-info').innerHTML +=
+      `<section class='new-trip-info'>
+          <p><a>Destination:</a> ${destinationInfo.destination}</p>
+          <p><a>Date:</a> ${trip.date}</p>
+          <p><a>Duration:</a> ${trip.duration} days</p>
+          <p><a>Status:</a> ${trip.status}</p>
+        </section>`
+    });
     console.log('displayAllTrips is running');
   },
+
+  displayApprovedTrips(travelerApprovedTrips, travelerDestinations) {
+    let destination = new Destination(travelerDestinations);
+    let separateTrips = travelerApprovedTrips.forEach(trip => {
+      let destinationInfo = destination.getDestinationDetails(trip.destinationID);
+      document.querySelector('.upcoming-trips-info').innerHTML +=
+      `<section class='new-trip-info'>
+          <p><a>Destination:</a> ${destinationInfo.destination}</p>
+          <p><a>Date:</a> ${trip.date}</p>
+          <p><a>Duration:</a> ${trip.duration} days</p>
+          <p><a>Status:</a> ${trip.status}</p>
+        </section>`
+    });
+    console.log('displayApprovedTrips is running');
+  },
+
+  displayPendingTrips(travelerPendingTrips, travelerDestinations) {
+    let destination = new Destination(travelerDestinations);
+    let separateTrips = travelerPendingTrips.forEach(trip => {
+      let destinationInfo = destination.getDestinationDetails(trip.destinationID);
+      document.querySelector('.pending-trips-info').innerHTML +=
+      `<section class='new-trip-info'>
+          <p><a>Destination:</a> ${destinationInfo.destination}</p>
+          <p><a>Date:</a> ${trip.date}</p>
+          <p><a>Duration:</a> ${trip.duration} days</p>
+          <p><a>Status:</a> ${trip.status}</p>
+        </section>`
+    });
+    console.log('displayPendingTrips is running');
+  },
+
+  displayCostSpent(traveler) {
+    let amountInDollars = traveler.amountSpent.toFixed(2)
+    document.querySelector('.traveler-expenses-amount').innerText = `$${amountInDollars}`;
+
+  }
 }
 
 export default domUpdates;
