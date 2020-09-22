acb57d1346e3c0ec7f4c10dd308687b60e24226a
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-09-22 14:56:20 -0600

    Attempted to refactor post request

--- src/fetch-requests.js
+++ src/fetch-requests.js
@@ -1,5 +1,6 @@
 import createData from './index.js'
 import domUpdates from './domUpdates.js'
+import Trip from './trip.js'
 
 let fetchRequests = {
   postData(traveler, travelersAmountInput, durationInput, dateInput) {
@@ -7,23 +8,25 @@ let fetchRequests = {
     let data = {
       id: Date.now(),
       userID: traveler.id,
-      travelers: travelersAmountInput.value,
-      date: new Date(dateInput.value),
-      duration: durationInput.value,
+      travelers: parseInt(travelersAmountInput.value),
+      date: dateInput.value,
+      duration: parseInt(durationInput.value),
       status: 'pending',
       suggestedActivities: [],
-      }
+    };
 
-    fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips', {
+    let dataToPost = JSON.stringify(data)
+
+    return fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips', {
       method: 'POST',
-      body: JSON.stringify(data),
+      body: dataToPost,
       headers: {
         'Content-Type':'application/json'
       }})
       .then(response => response.json())
-      .then(response => console.log('Resource with id '+data.id+' successfully posted, newResource: '+data))
-      .then(response => console.log(data))
-      .catch(err => console.log('There was an error posting this data.'))
+      .then(response => console.log('This is the data', data))
+      .then(response => console.log(`Resource with id ${data.id} successfully posted, newResource: ${JSON.stringify(data)}`))
+      .catch(err => console.log('There was an error posting this data.'));
   },
 
   getData() {
